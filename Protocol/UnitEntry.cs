e405c3168fd0fdd2275c7d7bb5d00b8196cb6aa7
Michiel Oda <97458010+MichielOda@users.noreply.github.com>
2024-01-08 15:14:52 +0100

    Add classes needed for the validator. (#2)
    * Add classes needed for the validator.
    
    * Make UnitList internal as it could cause issues when used via dependecy instead of directly.
    
    * CR remarks
    
    Adding editorconfig
    Format document on files & csproj
    Changed to inheritdoc + commit suggestion

---
+++ Protocol/UnitEntry.cs
@@ -0,0 +1,27 @@
+﻿namespace Skyline.DataMiner.XmlSchemas.Protocol
+{
+    using System.Collections.Generic;
+
+    internal class UnitEntry : IUnitEntry
+    {
+        public UnitEntry(string name, string value, bool ignoreInDescription, IList<string> legacyNotations)
+        {
+            Name = name;
+            Value = value;
+            IgnoreInDescription = ignoreInDescription;
+            LegacyNotations = legacyNotations;
+        }
+
+        /// <inheritdoc />
+        public string Name { get; }
+
+        /// <inheritdoc />
+        public string Value { get; }
+
+        /// <inheritdoc />
+        public bool IgnoreInDescription { get; }
+
+        /// <inheritdoc />
+        public IList<string> LegacyNotations { get; }
+    }
+}
